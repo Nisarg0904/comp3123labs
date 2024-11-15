f6a6d0cccc4566ee92356072bcda0b90725da36e
Nisarg Bhatti <nisarg.bhatti0904@gmail.com>
2024-11-14 20:35:30 -0500

    lab 11 done

---
+++ lab11_101410311_axios_example/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonList from "./PersonList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lab 11 - Axios Example</h1>
+      <PersonList />
+    </div>
+  );
+}
+
+export default App;
